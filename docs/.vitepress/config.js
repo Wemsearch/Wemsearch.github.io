776e53460dac1c33e5d9b051befab9ac3ee2265b
Wemsearch <164760085+Wemsearch@users.noreply.github.com>
2024-03-24 22:41:50 +0800

    Create config.js

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,16 @@
+export default {
+  // 网页设置
+  lang: 'zh-cmn-Hans',
+  title: 'Wemsearch',
+  description: 'Wemsur‘s Bing Search',
+  head: [['link', { rel: 'icon', href: 'https://wemsearch.github.io/bing.png' }]],
+  themeConfig: {
+    // theme-level options
+    logo: '/bing.png',
+    nav: [
+      { text: 'bing', link: 'https://cn.bing.com/' },
+      { text: 'bilibili', link: 'https://bilibili.com' },
+      { text: 'modrinth', link: 'https://modrinth.com' }
+    ]
+  }
+}
